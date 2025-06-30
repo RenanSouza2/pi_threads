32e3393530ea1ac5a7ed923d1da72a4cb6d014e1
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-06-30 17:31:47 -0300

    pi 1

--- src/main.c
+++ src/main.c
@@ -2,10 +2,14 @@
 
 #include "../mods/clu/header.h"
 
+#include "../lib/pi/header.h"
+
 // int main(int argc, char** argv)
 int main()
 {
     setbuf(stdout, NULL);
+
+    pi_threads(1000, 5);
 
     printf("\n");
     return 0;
