fd0194d3516626229ea5b7843ca4c0cd78ce62eb
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-07-02 22:35:08 -0300

    somewhat working

--- lib/pi/code.c
+++ lib/pi/code.c
@@ -313,14 +313,14 @@ fix_num_t pi_0(uint64_t size, uint64_t layer_count)
 
 fix_num_t pi_threads(uint64_t size)
 {
-    uint64_t layer_count = 3;
+    uint64_t layer_count = 2;
 
-    uint64_t max = 32 * size + 4;
-    uint64_t i_max = (max / layer_count) + 1;
-    uint64_t mid = i_max / 2;
+    uint64_t n_max = 32 * size + 4;
+    uint64_t i_max = (n_max / layer_count) + 1;
+    uint64_t i_mid = i_max / 2;
 
-    group_p g_1 = group_launch(size, layer_count,   1, mid, 0);
-    group_p g_2 = group_launch(size, layer_count, mid, max, 8);
+    group_p g_1 = group_launch(size, layer_count,     1, i_mid, 0);
+    group_p g_2 = group_launch(size, layer_count, i_mid, i_max, 8);
     
     fix_num_t fix_pi = pi_0(size, layer_count);
     fix_pi = fix_num_add(fix_pi, group_join(g_1));
--- src/main.c
+++ src/main.c
@@ -23,40 +23,10 @@ int main()
 {
     setbuf(stdout, NULL);
 
-    // // fix_num_t fix = pi_threads(20000);
-    // // printf("\n\n");
-    // // // fix_num_display_dec(fix);
-    // // fix_num_free(fix);
-
-    // uint64_t size = 20000;
-    
-    // uint64_t layer_count = 1;
-    // uint64_t max = 32 * size + 4;
-    // uint64_t i_max = (max / layer_count) + 1;
-    // i_max += i_max & 1;
-    // printf("\ni_max: %lu", i_max / 2);
-
-    // uint64_t begin = get_time();
-    // fix_num_t fix_1 = jumpstart_thread(i_max / 2, size, layer_count, 0, 0);
-    // uint64_t end = get_time();
-    // uint64_t time_1 = end - begin;
-    // printf("\ntime 1: %.2f", time_1 / 1e9);
-
-    // begin = get_time();
-    // fix_num_t fix_2 = jumpstart_standard(i_max / 2, size, layer_count);
-    // end = get_time();
-    // uint64_t time_2 = end - begin;
-    // printf("\ntime 2: %.2f", time_2 / 1e9);
-    
-    // assert(fix_num_cmp(fix_1, fix_2));
-
+    fix_num_t pi = pi_threads(3);
     printf("\n\n");
-    fix_num_t pi = pi_threads(1000);
     fix_num_display_dec(pi);
 
     printf("\n");
     return 0;
 }
-
-// size 20000 | time 0m33.542s | 0m21.630s
-// size 50000 | time 4m50.052s | 1m47.067s
