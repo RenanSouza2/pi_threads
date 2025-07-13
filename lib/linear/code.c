d7d657305c9257898945f5bc7871a00328083a7d
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-07-13 05:12:01 -0300

    Revert "failled version"
    This reverts commit 0cad629523869b5d21eed5936b78dd5d109a9064.

--- lib/linear/code.c
+++ lib/linear/code.c
@@ -328,41 +328,11 @@ void binary_splitting(union_num_t out[], uint64_t size, uint64_t i_0, uint64_t i
     union_num_t u_r_1 = union_num_mul(res_1[2], union_num_copy(res_2[1]));
     union_num_t u_r_2 = union_num_mul(res_2[2], union_num_copy(res_1[0]));
 
-    out[0] = union_num_mul(res_1[0], res_2[0]);
-    out[1] = union_num_mul(res_1[1], res_2[1]);
+    for(uint64_t i=0; i<2; i++)
+        out[i] = union_num_mul(res_1[i], res_2[i]);
+
     out[2] = union_num_add(u_r_1, u_r_2);
-    return;
-}
 
-// out vector length 2, returns Q, R in that order
-void binary_splitting_qr(union_num_t out[], uint64_t size, uint64_t i_0, uint64_t i_max)
-{
-    assert(i_0 <= i_max);
-    if(i_0 == i_max)
-    {
-        int64_t p = 2 * i_0 - 3;
-        int64_t q = 8 * i_0;
-        int64_t u = 1 - 2 * i_0;
-        int64_t v = 4 * i_0 + 2;
-
-        sig_num_t sig_q = sig_num_wrap(q * v);
-        sig_num_t sig_r = sig_num_wrap(p * u);
-
-        out[0] = union_num_wrap_sig(sig_q, size);
-        out[1] = union_num_wrap_sig(sig_r, size);
-        return;
-    }
-
-    uint64_t i_half = (i_0 + i_max) / 2;
-    union_num_t res_1[3], res_2[2];
-    binary_splitting(res_1, size, i_0       , i_half);
-    binary_splitting_qr(res_2, size, i_half + 1, i_max);
-
-    union_num_t u_r_1 = union_num_mul(res_1[2], union_num_copy(res_2[0]));
-    union_num_t u_r_2 = union_num_mul(res_2[1], union_num_copy(res_1[0]));
-
-    out[0] = union_num_mul(res_1[1], res_2[0]);
-    out[1] = union_num_add(u_r_1, u_r_2);
     return;
 }
 
@@ -371,11 +341,12 @@ float_num_t pi_v3(uint64_t size)
     uint64_t index_max = 32 * size + 4;
 
     union_num_t res[3];
-    binary_splitting_qr(res, size, 1, index_max);
+    binary_splitting(res, size, 1, index_max);
+    union_num_free(res[0]);
 
-    float_num_t flt_pi = union_num_unwrap_float(res[1]);
+    float_num_t flt_pi = union_num_unwrap_float(res[2]);
     flt_pi = float_num_mul_sig(flt_pi, sig_num_wrap(6));
-    flt_pi = float_num_div(flt_pi, union_num_unwrap_float(res[0]));
+    flt_pi = float_num_div(flt_pi, union_num_unwrap_float(res[1]));
     flt_pi = float_num_add(flt_pi, float_num_wrap(3, size));
     return flt_pi;
 }
