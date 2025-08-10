341a763b10ba0b7cdb6020f72c1d21f8cac662ef
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2025-08-10 05:00:58 -0300

    mid commit

--- src/main.c
+++ src/main.c
@@ -31,14 +31,16 @@ int main()
 {
     setbuf(stdout, NULL);
 
-    uint64_t size = 50000;
-    flt_num_t flt_pi = pi_big(size);
-    // // printf("\n\n");flt_num_display_dec(flt_pi);
-    flt_num_free(flt_pi);
-
+    // uint64_t size = 50000;
+    // flt_num_t flt_pi = pi_big(size);
+    // // // printf("\n\n");flt_num_display_dec(flt_pi);
+    // flt_num_free(flt_pi);
+    
+    uint64_t size = 165000000;
     // prepare(size, 20, 1950, 5035);
     // prepare(size, 25, 57, 157);
-    // prepare(size, 26, 5, 30);
+    prepare(size, 26, 30, 78);
+    // prepare(size, 27, 4, 5);
 
     printf("\n");
     return 0;
